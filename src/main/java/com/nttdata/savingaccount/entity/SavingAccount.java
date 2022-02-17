a6812272acd196d176a420aebb59b79fb14142a1
Emir Sanchez <emir2104@gmail.com>
2022-02-17 10:04:04 -0500

    first commit

---
+++ src/main/java/com/nttdata/savingaccount/entity/SavingAccount.java
@@ -0,0 +1,40 @@
+package com.nttdata.savingaccount.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Document(collection = "saving_account")
+@AllArgsConstructor
+@NoArgsConstructor
+public class SavingAccount {
+  
+  @Id
+  private String id;
+  @NotNull
+  private Customer customer;
+  @NotNull
+  private String cardNumber;
+  @NotNull
+  private Integer limitTransactions;
+  @NotNull
+  private Integer freeTransactions;
+  @NotNull
+  private Double commissionTransactions;
+  @NotNull
+  private Double balance;
+  private Double minAverageVip;
+  private LocalDateTime createAt;
+  private List<Managers> owners;
+  private List<Managers> signatories;
+
+}
